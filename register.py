39901f375081640362e5d1271634b2d70a4d9334
Shun <s_k_dpeg@icloud.com>
2020-12-25 14:54:42 +0900

    add access

--- register.py
+++ register.py
@@ -1,4 +1,3 @@
-from hello import SearchForm
 from flask import Flask, render_template, request,redirect
 from flask.globals import request
 import gspread
@@ -34,7 +33,7 @@ def Hello():
 @app.route('/register')
 def register():
     page = request.args.get('circle')
-    key_list = Worksheet.col_values(9)
+    key_list = Worksheet.col_values(9) #query parameter
     row_number = -1
     for i, key in enumerate(key_list):
         if key == page:
@@ -45,8 +44,27 @@ def register():
     space = Worksheet.cell(row_number,4).value
     univ = Worksheet.cell(row_number,5).value
     intro = Worksheet.cell(row_number,6).value
-    key = Worksheet.cell(row_number,8).value
-    return render_template('index.html',name=name,genre=genre,space=space,univ=univ,intro=intro,key=key)
+    key = Worksheet.cell(row_number,8).value #keywords to search
+    lineId = Worksheet.cell(row_number,10).value
+    webSite = Worksheet.cell(row_number,11).value
+    twitterId = Worksheet.cell(row_number,12).value
+    instagramId = Worksheet.cell(row_number,13).value
+    mailAddress = Worksheet.cell(row_number,15).value
+    otherWays = Worksheet.cell(row_number,14).value
+
+    return render_template('index.html',
+    name=name,
+    genre=genre,
+    space=space,
+    univ=univ,
+    intro=intro,
+    key=key,
+    lineId=lineId,
+    webSite=webSite,
+    twitterId=twitterId,
+    instagramId=instagramId,
+    mailAddress=mailAddress,
+    otherWays=otherWays,)
 
 @app.route('/submit', methods=('GET', 'POST'))
 def search():
