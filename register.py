ab82b366f756e7872a5f0652fcfdcb00107077cb
Wataru Nakata <wataru9871@gmail.com>
2020-12-25 13:59:06 +0900

    merged

--- register.py
+++ register.py
@@ -1,18 +1,31 @@
-from flask import Flask, render_template
+from hello import SearchForm
+from flask import Flask, render_template, request,redirect
 from flask.globals import request
 import gspread
+import os
 import json
 from gspread.models import Worksheet
 from oauth2client.client import Credentials
 from oauth2client.service_account import ServiceAccountCredentials
 
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 credentials = ServiceAccountCredentials.from_json_keyfile_name('prokiso-fd10525a5501.json',scope)
 gc = gspread.authorize(credentials)
 SPREADSHEET_KEY = '1eYy8_9XgbZjSWdPWX4wuruXKW3R6Cbv35wWILX_dyQc'
-Worksheet = gc.open_by_key(SPREADSHEET_KEY).worksheet("Sheet1")
+Worksheet = gc.open_by_key(SPREADSHEET_KEY)
+SECRET_KEY = os.urandom(32)
 
 app=Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
+
+# 検索フォームの設定
+class SearchForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+
 
 @app.route('/')
 def Hello():
@@ -34,6 +47,21 @@ def register():
     intro = Worksheet.cell(row_number,6).value
     key = Worksheet.cell(row_number,8).value
     return render_template('index.html',name=name,genre=genre,space=space,univ=univ,intro=intro,key=key)
+
+@app.route('/submit', methods=('GET', 'POST'))
+def search():
+    form = SearchForm()
+    if form.validate_on_submit():
+        print(form.name)
+        return redirect('/search_results/' + form.name.data)
+    return render_template('submit.html', form=form)
+
+@app.route('/search_results/<searchTerm>',methods=['GET', 'POST'])
+def show_search_results(searchTerm):
+    result = Worksheet.sheet1.findall(searchTerm)
+    return render_template('hello.html', name1=result)
+
+   
 
 if __name__ == "__main__":
     app.run(debug=True)
